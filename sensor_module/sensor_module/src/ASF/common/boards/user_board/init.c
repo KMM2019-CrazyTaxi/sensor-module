27594a250ddcbf3d3435b1533ec5769d8113a004
Herman Appelgren <herap603@student.liu.se>
2019-11-05 12:03:21 +0100

    Added initialization for I2C. Not tested on the microcontroller.

--- sensor_module/sensor_module/src/ASF/common/boards/user_board/init.c
+++ sensor_module/sensor_module/src/ASF/common/boards/user_board/init.c
@@ -16,11 +16,31 @@ static void basic_io_test_init(void)
 	__asm__ __volatile__ ("nop");
 }
 
+static void i2c_init(void)
+{
+	// Sätt på ström till TWI-krets.
+	PRR0 = PRR0 & ~(1 << PRTWI);
+	
+	// Konfigurera SCL-frekvens.
+	TWSR = 0x00; // Prescaler 1 ger grund på 1 MHz, så möjliga klockfrekvenser 3.9 - 1000 KHz (400 KHz max enl. protokoll).
+	TWBR = 50; // Ger SCL-frekvens 10 KHz.
+	
+	// Övrig konfigurering.
+	unsigned char twcr_value = 0;
+	twcr_value |= (0 << TWINT); // Clear interrupt bit.
+	twcr_value |= (0 << TWIE); // Don't use interrupt for I2C transmissions.
+	twcr_value |= (1 << TWEA); // Enable ACK when data received.
+	twcr_value |= (1 << TWEN); // Enable I2C circuit.
+	TWCR = twcr_value;	
+}
+
+static void communication_init(void)
+{
+	i2c_init();
+	// TODO Initialize SPI control registers.
+}
+
 void board_init(void)
 {
-	/* This function is meant to contain board-specific initialization code
-	 * for, e.g., the I/O pins. The initialization can rely on application-
-	 * specific board configuration, found in conf_board.h.
-	 */
-	basic_io_test_init();
+	communication_init();
 }
