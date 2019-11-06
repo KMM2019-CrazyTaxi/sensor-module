32cf265185cac65ec6955214cade5c1574cf8d71
Herman Appelgren <herman.appelgren@liu.se>
2019-11-06 15:04:26 +0100

    Added range finder API.

--- sensor_module/sensor_module/Debug/makedep.mk
+++ sensor_module/sensor_module/Debug/makedep.mk
@@ -2,6 +2,20 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+src\range_finder_api\core\src\vl53l0x_api.c
+
+src\range_finder_api\core\src\vl53l0x_api_calibration.c
+
+src\range_finder_api\core\src\vl53l0x_api_core.c
+
+src\range_finder_api\core\src\vl53l0x_api_ranging.c
+
+src\range_finder_api\core\src\vl53l0x_api_strings.c
+
+src\range_finder_api\platform\src\vl53l0x_platform.c
+
+src\range_finder_api\platform\src\vl53l0x_platform_log.c
+
 src\tests\basic_functionality_tests.c
 
 src\ASF\common\boards\user_board\init.c
---
+++ sensor_module/sensor_module/Debug/src/range_finder_api/platform/src/vl53l0x_platform.d
@@ -0,0 +1,54 @@
+src/range_finder_api/platform/src/vl53l0x_platform.d \
+ src/range_finder_api/platform/src/vl53l0x_platform.o: \
+ ../src/range_finder_api/platform/src/vl53l0x_platform.c \
+ ../src/range_finder_api/platform/incl/vl53l0x_platform.h \
+ ../src/range_finder_api/core/incl/vl53l0x_def.h \
+ ../src/range_finder_api/core/incl/vl53l0x_device.h \
+ ../src/range_finder_api/platform/incl/vl53l0x_types.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h \
+ ../src/range_finder_api/platform/incl/vl53l0x_platform_log.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\string.h \
+ ../src/range_finder_api/platform/incl/vl53l0x_i2c_platform.h \
+ ../src/range_finder_api/platform/incl/vl53l0x_i2c_platform.h \
+ ../src/range_finder_api/core/incl/vl53l0x_api.h \
+ ../src/range_finder_api/core/incl/vl53l0x_api_strings.h \
+ ../src/range_finder_api/core/incl/vl53l0x_def.h
+
+../src/range_finder_api/platform/incl/vl53l0x_platform.h:
+
+../src/range_finder_api/core/incl/vl53l0x_def.h:
+
+../src/range_finder_api/core/incl/vl53l0x_device.h:
+
+../src/range_finder_api/platform/incl/vl53l0x_types.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h:
+
+../src/range_finder_api/platform/incl/vl53l0x_platform_log.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\string.h:
+
+../src/range_finder_api/platform/incl/vl53l0x_i2c_platform.h:
+
+../src/range_finder_api/platform/incl/vl53l0x_i2c_platform.h:
+
+../src/range_finder_api/core/incl/vl53l0x_api.h:
+
+../src/range_finder_api/core/incl/vl53l0x_api_strings.h:
+
+../src/range_finder_api/core/incl/vl53l0x_def.h:
