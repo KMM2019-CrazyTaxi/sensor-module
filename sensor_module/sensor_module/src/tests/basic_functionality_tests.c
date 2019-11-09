f0af26b09232b9d6f77df1be1232987ccd6f34d4
Herman Appelgren <herap603@student.liu.se>
2019-11-09 22:11:23 +0100

    Changes to basic range finder test functions.

--- sensor_module/sensor_module/src/tests/basic_functionality_tests.c
+++ sensor_module/sensor_module/src/tests/basic_functionality_tests.c
@@ -33,34 +33,102 @@ void accelerator_communication_test(void)
 
 void range_finder_communication_test(void)
 {
+	VL53L0X_Error status;
 	uint32_t refSpadCount;
 	uint8_t isApertureSpads, VhvSettings, PhaseCal;
 	VL53L0X_RangingMeasurementData_t ranging_data;
 	VL53L0X_Dev_t dev;
 	
-	PORTA = 1;
-	VL53L0X_DataInit(&dev);
+	dev.I2cDevAddr = 0x52;
+	dev.comms_type = VL53L0X_COMMS_I2C;
+	dev.comms_speed_khz = 10;
+	VL53L0X_ResetDevice(&dev);
+	
+	PORTA = 0x01;
+	status = VL53L0X_DataInit(&dev);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF1;
+		while (1);
+	}
+	
 	PORTA = 2;
 	VL53L0X_StaticInit(&dev);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF2;
+		while (1);
+	}
+	
 	PORTA = 3;
 	VL53L0X_PerformRefSpadManagement(&dev, &refSpadCount, &isApertureSpads);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF3;
+		while (1);
+	}
+	
 	PORTA = 4;
 	VL53L0X_PerformRefCalibration(&dev, &VhvSettings, &PhaseCal);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF4;
+		while (1);
+	}
+	
 	PORTA = 5;
 	VL53L0X_SetDeviceMode(&dev, VL53L0X_DEVICEMODE_SINGLE_RANGING);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF5;
+		while (1);
+	}
+	
 	PORTA = 6;
 	VL53L0X_SetLimitCheckEnable(&dev, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF6;
+		while (1);
+	}
+	
 	PORTA = 7;
 	VL53L0X_SetLimitCheckEnable(&dev, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF7;
+		while (1);
+	}
+	
 	PORTA = 8;
 	VL53L0X_SetLimitCheckEnable(&dev, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, 1);
-	PORTA = 9;
-	VL53L0X_SetLimitCheckValue(&dev, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, (FixPoint1616_t)(1.5 * 0.023 * 65536));
-	PORTA = 10;
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF8;
+		while (1);
+	}
 	
+	PORTA = 9;
+	FixPoint1616_t f = 1.5;
+	f *= 0.023;
+	uint32_t i = 65536;
+	f *= i;
+	VL53L0X_SetLimitCheckValue(&dev, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, f);
+	if (status != VL53L0X_ERROR_NONE)
+	{
+		PORTA = 0xF9;
+		while (1);
+	}
+	
+	PORTA = 10;	
 	while (1)
 	{
 		VL53L0X_PerformSingleRangingMeasurement(&dev, &ranging_data);
-		PORTA = ranging_data.RangeMilliMeter;
+		if (status != VL53L0X_ERROR_NONE)
+		{
+			PORTA = 0xFA;
+			while (1);
+		}
+		PORTA = ranging_data.RangeMilliMeter >> 3;
 	}
 }
--- sensor_module/sensor_module/src/tests/basic_functionality_tests.h
+++ sensor_module/sensor_module/src/tests/basic_functionality_tests.h
@@ -13,6 +13,7 @@
 #include <stdint.h>
 #include "accelerator_module.h"
 #include "vl53l0x_api.h"
+#include "i2c.h"
 
 /*
  * Counts from 0 (inclusive) to count_to (non-inclusive) repeatedly on port A.
@@ -26,9 +27,17 @@ void run_regular_pulse_test(const uint8_t count_to);
  * the 8 MSB (including 2-complement bit) on PORTA.
  *
  * Assumes: Port A has been configure as output port.
+ *          I2C has been configured.
  */
 void accelerator_communication_test(void);
 
+/*
+ * Configures the range finder, continuously reads range and outputs the detected
+ * in ~cm in PORTA.
+ *
+ * Assumes: Port A has been configured as output port.
+ *          I2C has been configured.
+ */
 void range_finder_communication_test(void);
 
 #endif /* BASIC_IO_TEST_H_ */
