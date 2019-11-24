a74664c71a4d29e34fc802e8773d0acdc85555f9
Angus Lothian <angus.lothian@hotmail.com>
2019-11-24 10:50:45 +0100

    Remove debug port printing
    Remove debug port printing

--- sensor_module/sensor_module/src/ASF/common/boards/user_board/init.c
+++ sensor_module/sensor_module/src/ASF/common/boards/user_board/init.c
@@ -14,18 +14,18 @@
 static void io_pin_init(void)
 {
 	// Initiera io-portar.
-	DDRA = 0xFF; // Alla pinnar på A är output.
-	DDRB = 0x01; // PB0 är output for SHTDWN på range finder.
+	DDRA = 0xFF; // Alla pinnar pÃ¥ A Ã¤r output.
+	DDRB = 0x01; // PB0 Ã¤r output for SHTDWN pÃ¥ range finder.
 	__asm__ __volatile__ ("nop");
 }
 
 static void i2c_init(void)
 {
-	// Sätt på ström till TWI-krets.
+	// SÃ¤tt pÃ¥ strÃ¶m till TWI-krets.
 	PRR0 = PRR0 & ~(1 << PRTWI);
 	
 	// Konfigurera SCL-frekvens.
-	TWSR = 0x00; // Prescaler 1 ger grundfrekvens på 1 MHz, så möjliga klockfrekvenser 3.9 - 1000 KHz (400 KHz max enl. protokoll).
+	TWSR = 0x00; // Prescaler 1 ger grundfrekvens pÃ¥ 1 MHz, sÃ¥ mÃ¶jliga klockfrekvenser 3.9 - 1000 KHz (400 KHz max enl. protokoll).
 	TWBR = 2; // Grundfrekvensen delas med 2*TWBR.
 }
 
@@ -40,7 +40,6 @@ static void spi_slave_init(void)
 	
 	/* Enable SPI */
 	SPCR = (1<<SPE);
-	PORTA = 0xFF;
 }
 
 void board_init(void)
